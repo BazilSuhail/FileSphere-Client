172a9e0c5eeccf09edb85f3080d412d464074d36
MuhammdRehman <ammuhammadrehman@gmail.com>
2024-09-06 23:17:33 +0500

    Download Functionality added

--- client.c
+++ client.c
@@ -12,6 +12,12 @@
 void uploadFile(int clientSocket, const char *filePath)
 {
     
+    int val = send(clientSocket,"1",1,0);
+    if(val<0)
+    {
+        printf("There is an error in this please try again");
+        return;
+    }
     int fileDescriptor = open(filePath, O_RDONLY);
     if (fileDescriptor < 0)
     {
@@ -49,7 +55,71 @@ void uploadFile(int clientSocket, const char *filePath)
     response[sizeof(response) - 1] = '\0';
     printf("Server response: %s\n", response);
 }
+void DownloadFile(clientSocket,filename)
+{
+    int val = send(clientSocket,"2",1,0);
+    if(val<0)
+    {
+        printf("There is an error in Download please try again");
+        return;
+    }
+    char buffer[MAX_SIZE];
+    char command[256];
+    int recvSize = recv(clientSocket, command, sizeof(command) - 1, 0);
+    if (recvSize <= 0)
+    {
+        close(clientSocket);
+        return;
+    }
+    command[recvSize] = '\0';
 
+    if (strncmp(command, "$UPLOAD$", 8) != 0)
+    {
+        printf("Invalid command.\n");
+        close(clientSocket);
+        return;
+    }
+
+    char filePath[256];
+    strcpy(filePath, "test.txt");
+
+    int availableSpace = checkStorageSpace();
+    if (availableSpace <= 0)
+    {
+        send(clientSocket, "$FAILURE$LOW_SPACE$", 20, 0);
+        close(clientSocket);
+        return;
+    }
+
+    send(clientSocket, "$SUCCESS$", 9, 0);
+
+
+    int fileDescriptor = open(filePath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fileDescriptor < 0)
+    {
+        perror("Error creating file");
+        close(clientSocket);
+        return;
+    }
+
+    ssize_t bytesRead;
+    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0)
+    {
+        write(fileDescriptor, buffer, bytesRead);
+    }
+
+    if (bytesRead < 0)
+    {
+        perror("Error receiving file data");
+    }
+    else
+    {
+        send(clientSocket, "$SUCCESS$", 9, 0);
+    }
+
+    close(fileDescriptor);
+    close(clientSocket);
+}
 int main()
 {
     int clientSocket;
@@ -72,13 +142,23 @@ int main()
         close(clientSocket);
         return 1;
     }
+    int n;
+    printf("1.Upload\n2.Download : ");
+    scanf("%d",n);
+    if(n==1){
 
-    char filePath[256];
-    printf("Enter file path to upload: ");
-    scanf("%s", filePath);
+        char filePath[256];
+        printf("Enter file path to upload: ");
+        scanf("%s", filePath);
 
-    uploadFile(clientSocket, filePath);
-
+        uploadFile(clientSocket, filePath);
+    }
+    else{
+        char filename[30];
+        printf("Enter file path to Download: ");
+        scanf("%s", filename);
+        DownloadFile(clientSocket,filename);
+    }
     close(clientSocket);
     return 0;
 }
