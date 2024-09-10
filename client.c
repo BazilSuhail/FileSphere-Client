c1d4d0d73e1767492196389a94f2dfde1cee1582
BazilSuhail <bazil1854@gmail.com>
2024-09-11 01:27:06 +0500

    Added the file size limit checking for the user and prompted the server response according to the State !!

--- client.c
+++ client.c
@@ -17,7 +17,7 @@ int main()
 
     struct sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(12345);
+    serverAddr.sin_port = htons(8081);
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
@@ -29,7 +29,7 @@ int main()
     printf("Select an option:\n1. Register\n2. Authenticate\n");
     int option;
     scanf("%d", &option);
-    getchar(); // To consume the newline character left by scanf
+    getchar();
     send(clientSocket, &option, sizeof(option), 0);
 
     if (option == 1)
@@ -40,18 +40,10 @@ int main()
         scanf("%s", userName);
         send(clientSocket, userName, strlen(userName), 0);
 
-        char response[MAX_SIZE];
-        recv(clientSocket, response, sizeof(response) - 1, 0);
-        response[strlen(response)] = '\0'; // Ensure null termination
-        printf("%s\n", response);
-
-        if (strcmp(response, "User created") == 0)
-        {
-            char password[MAX_SIZE];
-            printf("Enter password: ");
-            scanf("%s", password);
-            send(clientSocket, password, strlen(password), 0);
-        }
+        char password[MAX_SIZE];
+        printf("Enter password: ");
+        scanf("%s", password);
+        send(clientSocket, password, strlen(password), 0);
     }
     else if (option == 2)
     {
@@ -61,21 +53,68 @@ int main()
         scanf("%s", userName);
         send(clientSocket, userName, strlen(userName), 0);
 
+        char password[MAX_SIZE];
+        printf("Enter password: ");
+        scanf("%s", password);
+        send(clientSocket, password, strlen(password), 0);
+
         char response[MAX_SIZE];
         recv(clientSocket, response, sizeof(response) - 1, 0);
-        response[strlen(response)] = '\0'; // Ensure null termination
-        printf("%s\n", response);
+        response[sizeof(response) - 1] = '\0';
+        printf("Server: %s\n", response);
 
         if (strcmp(response, "User found") == 0)
         {
-            char password[MAX_SIZE];
-            printf("Enter password: ");
-            scanf("%s", password);
-            send(clientSocket, password, strlen(password), 0);
+            printf("Select an option:\n1. Upload file\n2. Download file\n");
+            int fileOption;
+            scanf("%d", &fileOption);
+            getchar();
+            send(clientSocket, &fileOption, sizeof(fileOption), 0);
 
-            recv(clientSocket, response, sizeof(response) - 1, 0);
-            response[strlen(response)] = '\0'; // Ensure null termination
-            printf("%s\n", response);
+            if (fileOption == 1)
+            {
+                char fileName[MAX_SIZE];
+                printf("Enter file name: ");
+                scanf("%s", fileName);
+                send(clientSocket, fileName, strlen(fileName), 0);
+                long fileSize;
+                printf("Enter file size: ");
+                scanf("%ld", &fileSize);
+                send(clientSocket, &fileSize, sizeof(fileSize), 0);
+
+                printf("File name and size sent.\n");
+
+                char response[MAX_SIZE];
+                int bytesRead = recv(clientSocket, response, sizeof(response) - 1, 0);
+                if (bytesRead > 0)
+                {
+                    response[bytesRead] = '\0';
+                    printf("Server response: %s\n", response);
+
+                    if (strcmp(response, "Out of space.") == 0)
+                    {
+                        printf("Server indicates out of space.\n");
+                    }
+                    else if (strcmp(response, "Error parsing config file.") == 0 ||
+                             strcmp(response, "Error updating config file.") == 0 ||
+                             strcmp(response, "Error writing to config file.") == 0)
+                    {
+                        printf("Server error: %s\n", response);
+                    }
+                    else
+                    {
+                        printf("Server message: %s\n", response);
+                    }
+                }
+                else
+                {
+                    printf("Failed to receive server response.\n");
+                }
+            }
+            else if (fileOption == 2)
+            {
+                printf("File download functionality is not yet implemented.\n");
+            }
         }
     }
 
