f2c59f502e1566618a2122ad2f4b014f4e14eaaf
BazilSuhail <bazil1854@gmail.com>
2024-09-11 16:01:30 +0500

    Integrated File upload and download for client with authentication of the user and his uploaded files !!

--- client.c
+++ client.c
@@ -3,8 +3,126 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <fcntl.h>
 
 #define MAX_SIZE 1024
+
+
+void downloadFile(int clientSocket, const char *fileName)
+{ 
+    ssize_t sentBytes = send(clientSocket, fileName, strlen(fileName), 0);
+    if (sentBytes < 0)
+    {
+        perror("Error sending file name to server");
+        return;
+    }
+ 
+    char response[256];
+    ssize_t receivedBytes = recv(clientSocket, response, sizeof(response) - 1, 0);
+    if (receivedBytes < 0)
+    {
+        perror("Error receiving acknowledgment from server");
+        return;
+    }
+    response[receivedBytes] = '\0';
+ 
+    if (strcmp(response, "No file found") == 0)
+    {
+        printf("Server response: No file found\n");
+        return;
+    }
+
+    // Ensure the server is ready to send the file by checking "$READY$"
+    /*if (strcmp(response, "$READY$") == 0)
+    {
+        printf("Server is ready to send the file.\n");
+    }
+    else
+    {
+        printf("Unexpected server response: %s\n", response);
+        return;
+    }*/
+
+    int fileDescriptor = open(fileName, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fileDescriptor < 0)
+    {
+        perror("Error creating file");
+        return;
+    }
+
+    char buffer[MAX_SIZE];
+    ssize_t bytesRead;
+    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0)
+    {
+        if (write(fileDescriptor, buffer, bytesRead) != bytesRead)
+        {
+            perror("Error writing to file");
+            close(fileDescriptor);
+            return;
+        }
+    }
+
+    if (bytesRead < 0)
+    {
+        perror("Error receiving file data");
+    }
+    else
+    {
+        printf("File downloaded and saved successfully.\n");
+    }
+
+    close(fileDescriptor);
+}
+
+
+void uploadFile(int clientSocket, const char *filePath)
+{
+    int fileDescriptor = open(filePath, O_RDONLY);
+    if (fileDescriptor < 0)
+    {
+        perror("Error opening file");
+        return;
+    }
+ 
+    const char *fileName = strrchr(filePath, '/');
+    fileName = fileName ? fileName + 1 : filePath;
+
+    ssize_t sentBytes = send(clientSocket, fileName, strlen(fileName), 0);
+    if (sentBytes < 0)
+    {
+        perror("Error sending file name to server");
+        close(fileDescriptor);
+        return;
+    }
+
+    char response[256];
+    ssize_t receivedBytes = recv(clientSocket, response, sizeof(response) - 1, 0);
+    if (receivedBytes < 0)
+    {
+        perror("Error receiving acknowledgment from server");
+        close(fileDescriptor);
+        return;
+    }
+    response[receivedBytes] = '\0';
+
+    /*if (strcmp(response, "$READY$") != 0)
+    {
+        printf("Server is not ready to receive the file.\n");
+        close(fileDescriptor);
+        return;
+    }*/
+ 
+    char buffer[MAX_SIZE];
+    ssize_t bytesRead;
+    while ((bytesRead = read(fileDescriptor, buffer, sizeof(buffer))) > 0)
+    {
+        send(clientSocket, buffer, bytesRead, 0);
+    }
+
+    printf("File uploaded successfully.\n");
+    close(fileDescriptor);
+}
 
 int main()
 {
@@ -44,6 +162,9 @@ int main()
         printf("Enter password: ");
         scanf("%s", password);
         send(clientSocket, password, strlen(password), 0);
+
+
+        printf("\n$ USER IS REGISTERED $\n");
     }
     else if (option == 2)
     {
@@ -77,6 +198,16 @@ int main()
                 printf("Enter file name: ");
                 scanf("%s", fileName);
                 send(clientSocket, fileName, strlen(fileName), 0);
+
+                // file name path , baad ke liyee
+                int fileDescriptor = open(fileName, O_RDONLY);
+                if (fileDescriptor < 0)
+                {
+                    perror("File Not found | Error opening file :/");
+                    close(clientSocket);
+                    return 0;
+                }
+
                 long fileSize;
                 printf("Enter file size: ");
                 scanf("%ld", &fileSize);
@@ -104,6 +235,7 @@ int main()
                     else
                     {
                         printf("Server message: %s\n", response);
+                        uploadFile(clientSocket, fileName);
                     }
                 }
                 else
@@ -133,6 +265,7 @@ int main()
                     {
                         // Proceed with file download logic if file is found
                         printf("File '%s' is available for download.\n", fileName);
+                        downloadFile(clientSocket, fileName);
                         // Additional download logic would go here (e.g., file transfer)
                     }
                     else if (strcmp(response, "File not found.") == 0)
